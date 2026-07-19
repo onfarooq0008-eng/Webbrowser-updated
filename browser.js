6532dda73ac0a73314c8c0fdbf6ba20dd3517c1a
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-07-19 14:13:45 +0400

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 const { chromium } = require("playwright");
-
+const fs = require("fs");
 
 let browser;
 
@@ -8,14 +8,15 @@ async function start(){
 
 browser = await chromium.launch({
 
-    headless:true,
+headless:true,
 
-    args:[
-        "--no-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu",
-        "--single-process"
-    ]
+args:[
+"--no-sandbox",
+"--disable-dev-shm-usage",
+"--disable-gpu",
+"--no-zygote",
+"--disable-software-rasterizer"
+]
 
 });
 
@@ -26,30 +27,40 @@ browser = await chromium.launch({
 async function open(url){
 
 
-let context = await browser.newContext({
+let options = {};
 
-    storageState:"cookies.json"
 
-});
+if(fs.existsSync("cookies.json")){
+
+options.storageState = "cookies.json";
+
+}
+
+
+
+let context = await browser.newContext(options);
 
 
 let page = await context.newPage();
 
 
+
 await page.goto(url,{
 
-    waitUntil:"domcontentloaded",
+waitUntil:"domcontentloaded",
 
-    timeout:30000
+timeout:30000
 
 });
+
 
 
 await context.storageState({
 
-    path:"cookies.json"
+path:"cookies.json"
 
 });
+
 
 
 let html = await page.content();
@@ -57,11 +68,14 @@ let html = await page.content();
 
 await page.close();
 
+await context.close();
+
 
 return html;
 
 
 }
+
 
 
 module.exports={
